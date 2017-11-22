219f37d0eb1f4640de32455987290b477b9675da
leo <leo@leo.com>
2017-11-22 20:19:44 +0800

    add method say fuck

--- src/main/java/com/kateng/demo/GitDemo.java
+++ src/main/java/com/kateng/demo/GitDemo.java
@@ -2,6 +2,10 @@ package com.kateng.demo;
 
 public class GitDemo {
 	public static void main(String[] args) {
+		System.out.println("4444");
 		System.out.println("1111");
+	}
+	public void say() {
+		System.out.println("say fuck~!~!~");
 	}
 }
